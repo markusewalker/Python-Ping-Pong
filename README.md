100746fff5ccc27c5e9178dcea590c966f257520
Markus Walker <markus.walker.25@gmail.com>
2022-01-08 20:24:24 -0800

    Refactor the code to utilize functions

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 The classic game of Ping Pong written in the Python programming language. This project was inspired and referenced by https://github.com/srimani-programmer/Ping-Pong-Game. This adds onto to the referenced project by fixing miscellanous bugs and adding the following features:
 
 - Customizable player names
+- Customizable ping pong ball speed
 - Customizable background (by default, it is Sonic the Hedgehog)
 - Customizable final score so that there is a clear winner that achieves ultimate Ping Pong bragging rights
 
@@ -19,13 +20,13 @@ Although this project is cross-platform, you will need to ensure your environmen
 While `turtle` should be a pre-installed Python library, you will need to ensure your installation of Python 3 comes with the library.
 
 ### Running via Command-line
-1. Clone this repository to a local environment.
-2. Open your command-line and navigate to the downloaded folder (if needed, unzip first).
-3. Depending upon how your environment handles Python 3, you may do one of two things:
+1. Clone this repository to a local environment. Alternatively, download as a zip and unzip the folder.
+2. Open your command-line and navigate to the directory hosting the downloaded folder
+3. Depending upon how your environment handles Python 3, you may do one of two things to invoke:
      - `python python-ping-pong.py`
      - `python3 python-ping-pong.py`
 
-If you are on a GUI such as IDLE, run the module as any other Python file and that should work as well.
+If you are on a GUI such as IDLE or PyCharm, run the module as any other Python file and that should work as well.
 
 Here is a preview of the game once it is successfully launched:
 
--- python-ping-pong.py
+++ python-ping-pong.py
@@ -1,3 +1,4 @@
+#################################################################################
 # Authored By: Markus Walker
 # Inspired By: https://github.com/srimani-programmer/Ping-Pong-Game
 # Licensed By: Open Source
@@ -7,97 +8,131 @@
 # turte module. Bring a friend and settle once and for all who is
 # the alpha Pong player. Player 1 uses controls 'w' and 's', while
 # Player 2 uses controls Up and Down.
+#################################################################################
 
 import turtle, os, time
 
-# Function to move the left paddle up.
-def left_paddle_up():
-    y = main.left_paddle.ycor()
-    y += 50                         # Adjust the speed going up for the left paddle.
-    main.left_paddle.sety(y)
+#################################################################################
+# GLOBAL VARIABLES
+#################################################################################
+player_1_score = 0
+player_2_score = 0
+HBORDER = 360
+VBORDER = 250
+COLLISION1 = 340
+COLLISION2 = 350
+SHIFT = 40
+FINAL_SCORE = 10
 
-# Function to move the left paddle down.
-def left_paddle_down():
-    y = main.left_paddle.ycor()
-    y -= 50                         # Adjust the speed going down for the left paddle.
-    main.left_paddle.sety(y)
-
-# Function to move the right paddle up.
-def right_paddle_up():
-    y = main.right_paddle.ycor()
-    y += 50                          # Adjust the speed going up for the right paddle.
-    main.right_paddle.sety(y)
-
-# Function to move the right paddle down.
-def right_paddle_down():
-    y = main.right_paddle.ycor()
-    y -= 50                         # Adjust the speed going down for the left paddle.
-    main.right_paddle.sety(y)
-
-# Main function for the game.
-def main():
-    player_1_score = 0
-    player_2_score = 0
-
-    # Constant variables.
-    HBORDER = 360
-    VBORDER = 250
-    COLLISION1 = 340
-    COLLISION2 = 350
-    SHIFT = 40
-    FINAL_SCORE = 10
-
+#################################################################################
+# Function to start the game. Prompts for both player's names, initializes 
+# the separate window and tracks player's movements.
+#################################################################################
+def startGame():
     print("Welcome to Python Ping-Pong!")
-    player1 = input("Player 1, enter your name: ")
-    player2 = input("Player 2, enter your name: ")
+    startGame.player1 = input("Player 1, enter your name: ")
+    startGame.player2 = input("Player 2, enter your name: ")
     print("Let's play Python Ping-Pong!")
 
-    window = turtle.Screen() 
-    window.title("Python Ping-Pong") 
-    window.bgcolor("white")    
-    window.bgpic("sonic.png")       # Background photo of Sonic the Hedgehog because you gotta go fast....
-    window.setup(width=1000,height=1000)
-    window.tracer(0)
+    startGame.window = turtle.Screen() 
+    startGame.window.title("Python Ping-Pong") 
+    startGame.window.bgcolor("white")    
+    startGame.window.bgpic("sonic.png") 
+    startGame.window.setup(width=1000,height=1000)
+    startGame.window.tracer(0)
 
-    # Create left paddle and allow it to be referenced in other functions.
-    main.left_paddle = turtle.Turtle()
-    main.left_paddle.speed(0)
-    main.left_paddle.shape("square")
-    main.left_paddle.color("green")
-    main.left_paddle.shapesize(stretch_wid=4,stretch_len=1)
-    main.left_paddle.penup()
-    main.left_paddle.goto(-350,0)
+    startGame.window.listen()
+    startGame.window.onkeypress(left_paddle_up, "w")
+    startGame.window.onkeypress(left_paddle_down, "s")
+    startGame.window.onkeypress(right_paddle_up, "Up")
+    startGame.window.onkeypress(right_paddle_down, "Down")
 
-    # Create right paddle and allow it to be referenced in other functions.
-    main.right_paddle = turtle.Turtle()
-    main.right_paddle.speed(0)
-    main.right_paddle.shape("square")
-    main.right_paddle.shapesize(stretch_wid=4,stretch_len=1)
-    main.right_paddle.color("red")
-    main.right_paddle.penup()
-    main.right_paddle.goto(350,0)
+#################################################################################
+# Function to create left paddle.
+#################################################################################
+def leftPaddle():
+    leftPaddle.left_paddle = turtle.Turtle()
+    leftPaddle.left_paddle.speed(0)
+    leftPaddle.left_paddle.shape("square")
+    leftPaddle.left_paddle.color("green")
+    leftPaddle.left_paddle.shapesize(stretch_wid=4,stretch_len=1)
+    leftPaddle.left_paddle.penup()
+    leftPaddle.left_paddle.goto(-350,0)
 
-    # Create the actual Pong ball.
-    ball = turtle.Turtle()
-    ball.speed(0)
-    ball.shape("circle")
-    ball.color("black")
-    ball.penup()
-    ball.goto(0,0)
-    ball_dx = 5.5   # Change ball_dx and ball_dy to adjust the Pong ball's speed...
-    ball_dy = 5.5
+#################################################################################
+# Function to move the left paddle up.
+#################################################################################
+def left_paddle_up():
+    y = leftPaddle.left_paddle.ycor()
+    y += 50                         # Adjust the speed going up for the left paddle.
+    leftPaddle.left_paddle.sety(y)
 
-    # Create a pen that will keep track of the current score.
-    pen = turtle.Turtle()
-    pen.speed(0)
-    pen.color("blue")
-    pen.penup()
-    pen.hideturtle()
-    pen.goto(0,260)
-    pen.write("SCOREBOARD\n\n", align="center", font=("Arial", 24, "bold"))
-    pen.write(player1.upper() + ": 0                    " + player2.upper() + ": 0 ",align="center",font=("Arial", 24, "bold"))
+#################################################################################
+# Function to move the left paddle down.
+#################################################################################
+def left_paddle_down():
+    y = leftPaddle.left_paddle.ycor()
+    y -= 50                         # Adjust the speed going down for the left paddle.
+    leftPaddle.left_paddle.sety(y)
 
-    # Create a pen that will draw a vertical line, making the center line.
+#################################################################################
+# Function to create right paddle.
+#################################################################################
+def rightPaddle():
+    rightPaddle.right_paddle = turtle.Turtle()
+    rightPaddle.right_paddle.speed(0)
+    rightPaddle.right_paddle.shape("square")
+    rightPaddle.right_paddle.shapesize(stretch_wid=4,stretch_len=1)
+    rightPaddle.right_paddle.color("red")
+    rightPaddle.right_paddle.penup()
+    rightPaddle.right_paddle.goto(350,0)
+
+#################################################################################
+# Function to move the right paddle up.
+#################################################################################
+def right_paddle_up():
+    y = rightPaddle.right_paddle.ycor()
+    y += 50                          # Adjust the speed going up for the right paddle.
+    rightPaddle.right_paddle.sety(y)
+
+#################################################################################
+# Function to move the right paddle down.
+#################################################################################
+def right_paddle_down():
+    y = rightPaddle.right_paddle.ycor()
+    y -= 50                         # Adjust the speed going down for the left paddle.
+    rightPaddle.right_paddle.sety(y)
+
+#################################################################################
+# Function to create the Ping Pong ball.
+#################################################################################
+def makeBall():
+    makeBall.ball = turtle.Turtle()
+    makeBall.ball.speed(0)
+    makeBall.ball.shape("circle")
+    makeBall.ball.color("black")
+    makeBall.ball.penup()
+    makeBall.ball.goto(0,0)
+    makeBall.ball_dx = 5.5   # Change ball_dx and ball_dy to adjust the Pong ball's speed...
+    makeBall.ball_dy = 5.5
+
+#################################################################################
+# Function to create a pen that will keep track of the current score.
+#################################################################################
+def score():
+    score.pen = turtle.Turtle()
+    score.pen.speed(0)
+    score.pen.color("blue")
+    score.pen.penup()
+    score.pen.hideturtle()
+    score.pen.goto(0,260)
+    score.pen.write("SCOREBOARD\n\n", align="center", font=("Arial", 24, "bold"))
+    score.pen.write(startGame.player1.upper() + ": 0                    " + startGame.player2.upper() + ": 0 ",align="center",font=("Arial", 24, "bold"))
+
+#################################################################################
+# Function to create a pen that will draw a vertical line.
+#################################################################################
+def vertLineOne():
     vert_line = turtle.Turtle()
     vert_line.speed(0)
     vert_line.shape("square")
@@ -106,7 +141,10 @@ def main():
     vert_line.penup()
     vert_line.goto(0,-200)
 
-    # Create a pen that will draw a second vertical line, making the left border.
+#################################################################################
+# Function to create a pen that will draw a second vertical line.
+#################################################################################
+def vertLineTwo():
     vert2_line = turtle.Turtle()
     vert2_line.speed(0)
     vert2_line.shape("square")
@@ -115,7 +153,10 @@ def main():
     vert2_line.penup()
     vert2_line.goto(540,-200)
 
-    # Create a pen that will draw a third vertical line, making the right border.
+#################################################################################
+# Function to create a pen that will draw a third vertical line.
+#################################################################################
+def vertLineThree():
     vert3_line = turtle.Turtle()
     vert3_line.speed(0)
     vert3_line.shape("square")
@@ -124,7 +165,10 @@ def main():
     vert3_line.penup()
     vert3_line.goto(-500,-200)
 
-    # Create a pen that will draw a horizontal line, making the top center border.
+#################################################################################
+# Function to create a pen that will draw a horizontal line.
+#################################################################################
+def horizLineOne():
     horiz_line = turtle.Turtle()
     horiz_line.speed(0)
     horiz_line.shape("square")
@@ -133,7 +177,10 @@ def main():
     horiz_line.penup()
     horiz_line.goto(30,310)
 
-    # Create a pen that will draw a second horizontal line, making the bottom center border.
+#################################################################################
+# Function to create a pen that will draw a second horizontal line.
+#################################################################################
+def horizLineTwo():
     horiz2_line = turtle.Turtle()
     horiz2_line.speed(0)
     horiz2_line.shape("square")
@@ -142,91 +189,111 @@ def main():
     horiz2_line.penup()
     horiz2_line.goto(30,-380)
 
-    # Keyboard binding needed to track actual player movement.
-    window.listen()
-    window.onkeypress(left_paddle_up, "w")
-    window.onkeypress(left_paddle_down, "s")
-    window.onkeypress(right_paddle_up, "Up")
-    window.onkeypress(right_paddle_down, "Down")
-
+#################################################################################
+# Function that actually handles the Ping Pong game. Handles the game's
+# boundaries, collisions and when the game will end.
+#################################################################################
+def pingPong():
     # While loop to handle the actual game, including paddle boundaries, Pong ball boundaries and ending the game.
     while True:
-        window.update()
+        startGame.window.update()
 
         # Moving the ball horiztionally and vertically.
-        ball.setx(ball.xcor() + ball_dx)
-        ball.sety(ball.ycor() + ball_dy)
+        makeBall.ball.setx(makeBall.ball.xcor() + makeBall.ball_dx)
+        makeBall.ball.sety(makeBall.ball.ycor() + makeBall.ball_dy)
 
         # Setting up the vertical borders for the left paddle.
-        if ((main.left_paddle.ycor() > VBORDER)):
-            main.left_paddle.sety(VBORDER)
+        if ((leftPaddle.left_paddle.ycor() > VBORDER)):
+            leftPaddle.left_paddle.sety(VBORDER)
 
         # Setting up the vertical borders for the left paddle.
-        elif ((main.left_paddle.ycor() < -VBORDER)):
-            main.left_paddle.sety(-VBORDER)
+        elif ((leftPaddle.left_paddle.ycor() < -VBORDER)):
+            leftPaddle.left_paddle.sety(-VBORDER)
 
         # Setting up the vertical borders for the right paddle.
-        if ((main.right_paddle.ycor() > VBORDER)):
-            main.right_paddle.sety(VBORDER)
+        if ((rightPaddle.right_paddle.ycor() > VBORDER)):
+            rightPaddle.right_paddle.sety(VBORDER)
 
         # Setting up the vertical borders for the right paddle.
-        elif ((main.right_paddle.ycor() < -VBORDER)):
-            main.right_paddle.sety(-VBORDER)
+        elif ((rightPaddle.right_paddle.ycor() < -VBORDER)):
+            rightPaddle.right_paddle.sety(-VBORDER)
 
         # Setting up the vertical borders for the Pong ball.
-        if (ball.ycor() > VBORDER):
-            ball.sety(VBORDER)
-            ball_dy *= -1
+        if (makeBall.ball.ycor() > VBORDER):
+            makeBall.ball.sety(VBORDER)
+            makeBall.ball_dy *= -1
         
-        elif (ball.ycor() < -VBORDER):
-            ball.sety(-VBORDER)
-            ball_dy *= -1
+        elif (makeBall.ball.ycor() < -VBORDER):
+            makeBall.ball.sety(-VBORDER)
+            makeBall.ball_dy *= -1
             
         # Setting up the horizontal borders for the Pong ball.
-        if (ball.xcor() > HBORDER):   
-            ball.goto(0,0)
-            ball_dx *= -1
+        if (makeBall.ball.xcor() > HBORDER):   
+            makeBall.ball.goto(0,0)
+            makeBall.ball_dx *= -1
+            global player_1_score, player_2_score
             player_1_score += 1
-            pen.clear()
-            pen.write("SCOREBOARD\n\n", align="center", font=("Arial", 24, "bold"))
-            pen.write(player1.upper() + ": " + str(player_1_score) + "                  " + player2.upper() + ": " + str(player_2_score) + " ".format(player_1_score, player_2_score), align="center", font=('Arial',24,"bold"))
+
+            score.pen.clear()
+            score.pen.write("SCOREBOARD\n\n", align="center", font=("Arial", 24, "bold"))
+            score.pen.write(startGame.player1.upper() + ": " + str(player_1_score) + "                  " + startGame.player2.upper() + ": " + str(player_2_score) + " ".format(player_1_score, player_2_score), align="center", font=('Arial',24,"bold"))
             #os.system("afplay wall-hit-sound.wav&")   # TODO: Uncomment if you're on Linux or macOS to hear sound.
 
-        if (ball.xcor()) < -HBORDER:
-            ball.goto(0,0)
-            ball_dx = ball_dx * -1
+        if (makeBall.ball.xcor()) < -HBORDER:
+            makeBall.ball.goto(0,0)
+            makeBall.ball_dx *= -1
             player_2_score += 1
-            pen.clear()
-            pen.write("SCOREBOARD\n\n", align="center", font=("Arial", 24, "bold"))
-            pen.write(player1.upper() + ": " + str(player_1_score) + "                  " + player2.upper() + ": " + str(player_2_score) + " ".format(player_1_score, player_2_score), align="center", font=('Arial',24,"bold"))
+
+            score.pen.clear()
+            score.pen.write("SCOREBOARD\n\n", align="center", font=("Arial", 24, "bold"))
+            score.pen.write(startGame.player1.upper() + ": " + str(player_1_score) + "                  " + startGame.player2.upper() + ": " + str(player_2_score) + " ".format(player_1_score, player_2_score), align="center", font=('Arial',24,"bold"))
             #os.system("afplay wall-hit-sound.wav&")   # TODO: Uncomment if you're on Linux or macOS to hear sound.
 
         # Handling the collisions with paddles.
-        if (ball.xcor() > COLLISION1) and (ball.xcor() < COLLISION2) and (ball.ycor() < main.right_paddle.ycor() + SHIFT and ball.ycor() > main.right_paddle.ycor() - SHIFT):
-            ball.setx(COLLISION1)
-            ball_dx *= -1
+        if (makeBall.ball.xcor() > COLLISION1) and (makeBall.ball.xcor() < COLLISION2) and (makeBall.ball.ycor() < rightPaddle.right_paddle.ycor() + SHIFT and makeBall.ball.ycor() > rightPaddle.right_paddle.ycor() - SHIFT):
+            makeBall.ball.setx(COLLISION1)
+            makeBall.ball_dx *= -1
             #os.system("afplay paddle-sound.wav&")    #TODO: Uncomment if you're on Linux or macOS to hear sound.
 
-        elif (ball.xcor() < -COLLISION1) and (ball.xcor() > -COLLISION2) and (ball.ycor() < main.left_paddle.ycor() + SHIFT and ball.ycor() > main.left_paddle.ycor() - SHIFT):
-            ball.setx(-COLLISION1)
-            ball_dx *= -1
+        elif (makeBall.ball.xcor() < -COLLISION1) and (makeBall.ball.xcor() > -COLLISION2) and (makeBall.ball.ycor() < leftPaddle.left_paddle.ycor() + SHIFT and makeBall.ball.ycor() > leftPaddle.left_paddle.ycor() - SHIFT):
+            makeBall.ball.setx(-COLLISION1)
+            makeBall.ball_dx *= -1
             #os.system("afplay paddle-sound.wav&")    # TODO: Uncomment if you're on Linux or macOS to hear sound.
         
         # Ends the game once the score has been reached.
         if (player_1_score < FINAL_SCORE and player_2_score < FINAL_SCORE):
             continue
         elif (player_1_score == FINAL_SCORE and player_2_score != FINAL_SCORE):
-            ball_dx = 0
-            pen.clear()
-            pen.write(player1 + " wins!", align="center",font=("Arial",24,"bold"))
+            makeBall.ball_dx = 0
+            score.pen.clear()
+            score.pen.write(startGame.player1 + " wins!", align="center",font=("Arial",24,"bold"))
+
             time.sleep(5)
             break
         elif (player_2_score == FINAL_SCORE and player_1_score != FINAL_SCORE):
-            ball_dx = 0
-            pen.clear()
-            pen.write(player2 + " wins!", align="center",font=("Arial",24,"bold"))
+            makeBall.ball_dx = 0
+            score.pen.clear()
+            score.pen.write(startGame.player2 + " wins!", align="center",font=("Arial",24,"bold"))
+
             time.sleep(5)
             break
 
+#################################################################################
+# Main function for the game.
+#################################################################################
+def main():
+    startGame()
+    leftPaddle()
+    rightPaddle()
+    makeBall()
+    score()
+    vertLineOne()
+    vertLineTwo()
+    vertLineThree()    
+    horizLineOne()
+    horizLineTwo()
+    pingPong()
+
+# Allow interpreter to recognize what to run in a "Pythonic" way.
 if __name__ == "__main__":
     main()
